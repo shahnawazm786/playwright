01cbdb485a72a54d79c7268340b1679a0ef04f77
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-30 15:09:59 +0530

    playright tutorial

---
+++ src/main/java/org/example/ExcelReadingDataExample.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.File;
+
+public class ExcelReadingDataExample {
+    public static void main(String[] args) {
+        File excel=new File("src/main/resournces/employee.xlsx");
+
+    }
+}
