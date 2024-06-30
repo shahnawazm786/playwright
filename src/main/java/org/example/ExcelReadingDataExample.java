d50fcde7d8bcad36c63ddc4fb590b1f1890d9a54
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-30 15:14:44 +0530

    playright tutorial

--- src/main/java/org/example/ExcelReadingDataExample.java
+++ src/main/java/org/example/ExcelReadingDataExample.java
@@ -1,10 +1,25 @@
 package org.example;
 
-import java.io.File;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.*;
 
 public class ExcelReadingDataExample {
     public static void main(String[] args) {
         File excel=new File("src/main/resournces/employee.xlsx");
+        FileInputStream inputStream=null;
+        HSSFWorkbook workbook=null;
+        try {
+             inputStream= new FileInputStream(excel);
+        }catch (FileNotFoundException exception){
+            exception.printStackTrace();
+            System.out.println("File not found exception "+exception.getMessage());
+        }
+        try{
+        workbook=new HSSFWorkbook(inputStream);}catch (IOException ioException){
+            ioException.printStackTrace();
+            System.out.println("Reading file is difficult"+ioException.getMessage());
+        }
 
     }
 }
