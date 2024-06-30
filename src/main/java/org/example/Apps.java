52777e6993d00a53f338d5336c3765fc9ccf803d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-30 16:26:11 +0530

    excel and log4j2

--- src/main/java/org/example/Apps.java
+++ src/main/java/org/example/Apps.java
@@ -2,18 +2,27 @@ package org.example;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
 import java.util.regex.Pattern;
+import java.util.zip.DataFormatException;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class Apps {
+    private static final Logger log= LogManager.getLogger(Apps.class);
     public static void main(String[] args) throws InterruptedException {
+        log.info("Playwright --");
         try (Playwright playwright = Playwright.create()) {
+            log.info("browser type");
             BrowserType chromium = playwright.chromium();
+            log.info("browser chromium");
+            log.info("browser launch");
             Browser browser = chromium.launch(
                     new BrowserType.LaunchOptions().setHeadless(false)); // true be default - headless
+            log.info("browser launched....");
             System.out.println(browser.contexts().size());
             System.out.println(browser.isConnected());
             BrowserContext context=browser.newContext();
